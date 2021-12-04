33918ce79ed1d8fdef1558da88d96d7901a61a76
gluspier <soimec@protonmail.com>
2021-12-04 12:12:39 +0000

    Updates to NFT royalty program
    * Fixed a bug that allowed "buying" the NFT without owner's
      signature/involvement
    * Added some globals to track current owner and royalty divisor
    * Added compile functions for deployment, using akita_inu_asa_utils

--- contracts/nft_royalties/program.py
+++ contracts/nft_royalties/program.py
@@ -1,15 +1,20 @@
+from akita_inu_asa_utils import *
 from pyteal import *
 
-def transfer_approval(royalty_percent, royalty_payouts):
+
+def transfer_approval(asset_id, royalty_percent, royalty_payouts):
     ratio_sum = sum([r["ratio"] for r in royalty_payouts])
 
     indices = range(len(royalty_payouts))
+
+    owner = Bytes("owner")
+    royalty = Bytes("royalty")
 
     default_frozen = AssetParam.defaultFrozen(Int(0))
     clawback = AssetParam.clawback(Int(0))
     manager = AssetParam.manager(Int(0))
     freeze = AssetParam.freeze(Int(0))
-    royalty_minimum = Gtxn[1].amount() / Int(royalty_percent) # might store in scratch?
+    royalty_minimum = Gtxn[1].amount() / App.globalGet(royalty) # might store in scratch?
 
     def payRoyalty(receiver):
         return Seq(
@@ -25,6 +30,12 @@ def transfer_approval(royalty_percent, royalty_payouts):
             ),
             InnerTxnBuilder.Submit(),
         )
+
+    on_create = Seq(
+        App.globalPut(owner, Txn.sender()),
+        App.globalPut(royalty, Int(int(100 / royalty_percent))),
+        Approve()
+    )
 
     # Gtxn[0]
     #     * Asset transfer via clawback, must be signed with escrow's
@@ -58,6 +69,7 @@ def transfer_approval(royalty_percent, royalty_payouts):
 
                 Gtxn[0].fee() == Int(0),
                 Gtxn[0].asset_amount() == Int(1),
+                Gtxn[0].xfer_asset() == Int(asset_id),
 
                 Gtxn[0].close_remainder_to() == Global.zero_address(),
                 Gtxn[0].rekey_to() == Global.zero_address(),
@@ -67,6 +79,8 @@ def transfer_approval(royalty_percent, royalty_payouts):
                 Gtxn[2].rekey_to() == Global.zero_address(),
                 Gtxn[3].close_remainder_to() == Global.zero_address(),
                 Gtxn[3].rekey_to() == Global.zero_address(),
+
+                Txn.sender() == Gtxn[0].asset_sender(),
 
                 default_frozen.hasValue(),
                 default_frozen.value(),
@@ -87,13 +101,19 @@ def transfer_approval(royalty_percent, royalty_payouts):
             )
         ),
         Seq(*[payRoyalty(royalty_payouts[i]) for i in indices]),
+        App.globalPut(owner, Gtxn[0].asset_receiver()),
         Approve()
     )
 
-    on_delete = Return(Txn.sender() == Global.creator_address())
+    on_delete = Return(
+        And(
+            Txn.sender() == Global.creator_address(),
+            Txn.sender() == App.globalGet(owner)
+        )
+    )
 
     program = Cond(
-        [Txn.application_id() == Int(0), Approve()],
+        [Txn.application_id() == Int(0), on_create],
         [Txn.on_completion() == OnComplete.NoOp, on_call],
         [Txn.on_completion() == OnComplete.DeleteApplication, on_delete],
         [Int(1), Reject()]
@@ -101,7 +121,7 @@ def transfer_approval(royalty_percent, royalty_payouts):
     return program
 
 
-def escrow_approval(transfer_app_id = 121):
+def escrow_approval(transfer_app_id=121):
     # Escrow contract/account
     on_call = Seq(
         Assert(
@@ -121,23 +141,38 @@ def escrow_approval(transfer_app_id = 121):
     return program
 
 
-if __name__ == "__main__":
-    royalty_percent = 10
-    royalty_payouts = [
-            {
-                "address": "ECNHJST6SMCFXH6R6Z2L4D4ZYHG6FZFULY7O3XFTXORWBRZJUF2MSQRGZA",
-                "ratio": 6
-            },
-            {
-                "address": "XFNIWS5CZ67DKPR5767PR3YIWDZFYSFNLPPEAE6YKFIMUUIZC7WGYK22IA",
-                "ratio": 4
-            },
-        ]
+def clear_program():
+    return Reject()
 
-    with open("escrow.teal", "w") as f:
-        compiled = compileTeal(escrow_approval(), mode=Mode.Application, version=5)
-        f.write(compiled)
 
-    with open("transfer_split.teal", "w") as f:
-        compiled = compileTeal(transfer_approval(royalty_percent, royalty_payouts), mode=Mode.Application, version=5)
-        f.write(compiled)
+def compile_transfer_app(algod_client, asset_id, royalty_percent, royalty_payouts):
+    check_build_dir()
+
+    if not os.path.exists('build/nft_royalties'):
+        os.mkdir('build/nft_royalties')
+
+    compile_program(
+            algod_client,
+            compileTeal(
+                transfer_approval(asset_id, royalty_percent, royalty_payouts),
+                mode=Mode.Application,
+                version=5),
+            'nft_royalties/transfer.compiled')
+    compile_program(
+            algod_client,
+            compileTeal(clear_program(), mode=Mode.Application, version=5),
+            'nft_royalties/clear.compiled')
+
+    write_schema("nft_royalties/globalSchema", 1, 1)
+    write_schema("nft_royalties/localSchema", 0, 0)
+
+
+def compile_escrow_app(algod_client, transfer_app_id):
+    teal = compileTeal(
+                escrow_approval(transfer_app_id),
+                mode=Mode.Signature,
+                version=5
+            )
+    with open("build/nft_royalties/escrow.teal", "w") as f:
+        f.write(teal)
+    return compile_program(algod_client, teal, "nft_royalties/escrow.compiled")
