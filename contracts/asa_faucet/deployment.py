9318cf024ba5b85dc330f248ea80dcd1a465f666
Samantha Palmer <samanthaxpalmer@gmail.com>
2021-12-11 22:13:17 -0500

    asa_faucet

---
+++ contracts/asa_faucet/deployment.py
@@ -0,0 +1,70 @@
+from algosdk.future import transaction
+from algosdk import account, mnemonic, logic, encoding
+from akita_inu_asa_utils import *
+from pyteal import *
+
+
+def deploy_app(client, private_key, approval_program, clear_program, global_schema, local_schema, app_args):
+    # define sender as creator
+    public_key = account.address_from_private_key(private_key)
+
+    # declare on_complete as NoOp
+    on_complete = transaction.OnComplete.NoOpOC.real
+
+    # get node suggested parameters
+    params = client.suggested_params()
+
+    signed_txn, tx_id = create_app_signed_txn(private_key,
+                                              public_key,
+                                              params,
+                                              on_complete,
+                                              approval_program,
+                                              clear_program,
+                                              global_schema,
+                                              local_schema,
+                                              app_args)
+
+    # send transaction
+    client.send_transactions([signed_txn])
+
+    wait_for_txn_confirmation(client, tx_id, 5)
+
+    # display results
+    transaction_response = client.pending_transaction_info(tx_id)
+    app_id = transaction_response['application-index']
+    print("Deployed new app-id: " + str(app_id))
+    print("Deployed application address: " +
+          encoding.encode_address(encoding.checksum(b'appID' + app_id.to_bytes(8, 'big'))))
+
+    return app_id
+
+
+# asset_id is the ASAs ID
+# end time is the time to allow withdrawel from escrow
+def deploy(algod_address, algod_token, creator_mnemonic, asset_id, end_time):
+
+    private_key = mnemonic.to_private_key(creator_mnemonic)
+    public_key = account.address_from_private_key(private_key)
+    algod_client = get_algod_client(algod_token,
+                                    algod_address)
+
+    approval_program = load_compiled(file_path='asset_timed_vault_approval.compiled')
+    clear_program = load_compiled(file_path='asset_timed_vault_clear.compiled')
+
+    global_schema = load_schema(file_path='globalSchema')
+    local_schema = load_schema(file_path='localSchema')
+
+    app_args = [
+        asset_id.to_bytes(8, "big"),
+        encoding.decode_address(public_key),
+        end_time.to_bytes(8, "big")
+    ]
+
+    app_id = deploy_app(algod_client,
+                        private_key,
+                        approval_program,
+                        clear_program,
+                        global_schema,
+                        local_schema,
+                        app_args)
+    return app_id
---
+++ contracts/asa_faucet/program.py
@@ -0,0 +1,109 @@
+
+from akita_inu_asa_utils import *
+from pyteal import *
+
+from pyteal import *
+import sys
+
+def approval_program():
+    # Keys for the global data stored by this smart contract.
+
+    # AssetID that this smart contract will drip.
+    asset_id_key = Bytes("asset_id")
+    # Time in seconds to allow drip
+    drip_time = Bytes("drip_time")
+    drip_amount = Bytes("drip_amount")
+
+    min_algo_amount = Bytes("min_algo_amount")
+    min_asset_amount = Bytes("min_asset_amount")
+
+    # keys for local data stored by this smart contract
+    user_last_claim_time = Bytes("last_claim_time")
+
+    @Subroutine(TealType.none)
+    def on_get_drip():
+        asset_holding = And(AssetHolding.balance(Txn.sender(), App.globalGet(asset_id_key)),
+                            Balance(Txn.sender()) >= min_algo_amount)
+        assert_fees = Assert(And(Global.group_size() == Int(2), Gtxn[0].amount() == Int(1000)))
+        assert_time = Assert(Global.latest_timestamp() - user_last_claim_time >= drip_time)
+        assert_has_volume = Assert(AssetHolding.balance(Global.current_application_address(), asset_id_key))
+
+        claim = Seq(
+                    InnerTxnBuilder.Begin(),
+                    InnerTxnBuilder.SetFields(
+                        {
+                            TxnField.asset_receiver: Txn.sender(),
+                            TxnField.type_enum: TxnType.AssetTransfer,
+                            TxnField.xfer_asset: App.globalGet(asset_id_key),
+                            TxnField.asset_amount: App.globalGet(drip_amount),
+                        }
+                    ),
+                    InnerTxnBuilder.Submit(),
+                    App.localPut(Int(0), user_last_claim_time, Global.latest_timestamp()),
+                    Approve()
+        )
+        return Cond([And(asset_holding, assert_fees, assert_time, assert_has_volume), claim])
+
+    on_create = Seq(
+        App.globalPut(asset_id_key, Btoi(Txn.application_args[0])),
+        App.globalPut(drip_time, Btoi(Txn.application_args[1])),
+        App.globalPut(min_algo_amount, Btoi(Txn.application_args[2])),
+        App.globalPut(min_asset_amount, Btoi(Txn.application_args[3])),
+        Approve(),
+    )
+
+    on_delete = Seq(
+        Assert(Txn.sender() == Global.creator_address()),
+        Approve(),
+    )
+
+    on_opt_in = Seq(
+        App.localPut(Int(0), user_last_claim_time, Global.latest_timestamp()),
+        Approve()
+    )
+
+    # Application router for this smart contract.
+    program = Cond(
+        [
+            Or(
+                # Your fees shouldn't exceed an unreasonable amount
+                Txn.fee() > Int(4000),
+                # No rekeys allowed (just to be safe)
+                Txn.rekey_to() != Global.zero_address(),
+                # This smart contract cannot be closed out.
+                Txn.on_completion() == OnComplete.CloseOut,
+                # This smart contract cannot be updated.
+                Txn.on_completion() == OnComplete.UpdateApplication,
+                # This smart contract cannot be cleared of local state
+                Txn.on_completion() == OnComplete.ClearState,
+            ),
+            Reject(),
+        ],
+
+        [Txn.application_id() == Int(0), on_create],
+        [Txn.on_completion() == OnComplete.OptIn, on_opt_in],
+        [Txn.on_completion() == OnComplete.NoOp, on_get_drip],
+        [Txn.on_completion() == OnComplete.DeleteApplication, on_delete],
+    )
+
+    return compileTeal(program, Mode.Application, version=5)
+
+
+def clear_program():
+    return compileTeal(Reject(), Mode.Application, version=5)
+
+
+def compile_app(algod_client):
+    dump_teal_assembly('asa_faucet_approval.teal', approval_program)
+    dump_teal_assembly('asa_faucet_clear.teal', clear_program)
+
+    compile_program(algod_client, approval_program(), 'asa_faucet_approval.compiled')
+    compile_program(algod_client, clear_program(), 'asa_faucet_clear.compiled')
+
+    write_schema(file_path='localSchema',
+                 num_ints=0,
+                 num_bytes=0)
+    write_schema(file_path='globalSchema',
+                 num_ints=3,
+                 num_bytes=3)
+
