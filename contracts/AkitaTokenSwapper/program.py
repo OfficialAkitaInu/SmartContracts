4a852c93f7338c7655d574a9b42aff49e8b19bca
Samantha Palmer <samanthaxpalmer@gmail.com>
2022-01-13 22:00:21 -0500

    rewrite swapper history, with the github workflow changes

---
+++ contracts/AkitaTokenSwapper/program.py
@@ -0,0 +1,140 @@
+from akita_inu_asa_utils import *
+
+from pyteal import *
+
+
+def approval_program():
+
+    swap_asset_ID = Bytes("Swap_Asset_ID")
+    new_asset_ID = Bytes("New_Asset_ID")
+    multiplier = Bytes("Multiply")
+
+    asset_decimal = AssetParam.decimals(App.globalGet(new_asset_ID))
+
+    @Subroutine(TealType.none)
+    def opt_into_asset(asset_id):
+       return Seq(
+           InnerTxnBuilder.Begin(),
+           InnerTxnBuilder.SetFields
+            (
+               {
+                   TxnField.type_enum: TxnType.AssetTransfer,
+                   TxnField.xfer_asset: asset_id,
+                   TxnField.asset_receiver: Global.current_application_address(),
+               }
+           ),
+           InnerTxnBuilder.Submit(),
+       )
+
+    @Subroutine(TealType.none)
+    def swap_token(amount_to_swap):
+        return Seq(
+            InnerTxnBuilder.Begin(),
+            InnerTxnBuilder.SetFields(
+                {
+                    TxnField.type_enum: TxnType.AssetTransfer,
+                    TxnField.asset_receiver: Txn.sender(),
+                    TxnField.asset_amount: amount_to_swap,
+                    TxnField.xfer_asset: App.globalGet(new_asset_ID),
+                }
+            ),
+            InnerTxnBuilder.Submit(),
+        )
+
+    opt_in_assets = Seq(
+        Assert(And(
+                   App.globalGet(swap_asset_ID) == Int(0),
+                   App.globalGet(new_asset_ID) == Int(0),
+                   Global.group_size() == Int(2),
+                   Gtxn[0].type_enum() == TxnType.Payment,
+                   Gtxn[0].receiver() == Global.current_application_address(),
+                   Gtxn[0].sender() == Global.creator_address(),
+                   Gtxn[0].amount() == Int(302000),
+                   Gtxn[1].application_id() == Global.current_application_id(),
+                   Gtxn[1].on_completion() == OnComplete.NoOp)
+               ),
+
+        opt_into_asset(Btoi(Txn.application_args[0])),
+        opt_into_asset(Btoi(Txn.application_args[1])),
+        App.globalPut(swap_asset_ID, Btoi(Txn.application_args[0])),
+        App.globalPut(new_asset_ID, Btoi(Txn.application_args[1])),
+        asset_decimal,
+        Assert(asset_decimal.hasValue()),
+        App.globalPut(multiplier, Exp(Int(10),asset_decimal.value())),
+        Approve()
+    )
+
+    swap = Seq(
+        Assert(And(Global.group_size() == Int(3),
+                   Gtxn[0].type_enum() == TxnType.AssetTransfer,
+                   Gtxn[0].xfer_asset() == App.globalGet(new_asset_ID),
+                   Gtxn[0].asset_amount() == Int(0),
+                   Gtxn[0].sender() == Gtxn[0].asset_receiver(),
+                   Gtxn[0].rekey_to() == Global.zero_address(),
+                   Gtxn[0].close_remainder_to() == Global.zero_address(),
+                   Gtxn[0].asset_close_to() == Global.zero_address(),
+
+                   Gtxn[1].type_enum() == TxnType.AssetTransfer,
+                   Gtxn[1].asset_amount() > Int(0),
+                   Gtxn[1].xfer_asset() == App.globalGet(swap_asset_ID),
+                   Gtxn[1].sender() == Gtxn[0].sender(),
+                   Gtxn[1].asset_receiver() == Global.current_application_address(),
+                   Gtxn[1].rekey_to() == Global.zero_address(),
+                   Gtxn[1].close_remainder_to() == Global.zero_address(),
+                   Gtxn[1].asset_close_to() == Global.zero_address(),
+
+                   Gtxn[2].on_completion() == OnComplete.NoOp,
+                   Gtxn[2].application_args.length() == Int(0),
+                   Gtxn[2].sender() == Gtxn[1].sender())),
+        swap_token(Gtxn[1].asset_amount() * App.globalGet(multiplier)),
+        Approve()
+    )
+
+    # Application router for this smart contract.
+    program = Cond(
+        [
+            Or(
+                # Your fees shouldn't exceed an unreasonable amount
+                Txn.fee() > Int(2000),
+                # No rekeys allowed
+                Txn.rekey_to() != Global.zero_address(),
+                # This smart contract cannot be closed out.
+                Txn.on_completion() == OnComplete.CloseOut,
+                # This smart contract cannot be updated.
+                Txn.on_completion() == OnComplete.UpdateApplication,
+                # This smart contract cannot be cleared of local state
+                Txn.on_completion() == OnComplete.ClearState,
+                # This smart contract cannot be deleted
+                Txn.on_completion() == OnComplete.DeleteApplication,
+                # This smart contract cannot be opted in to
+                Txn.on_completion() == OnComplete.OptIn,
+                # no closing out algo allowed
+                Txn.close_remainder_to() != Global.zero_address(),
+                Txn.asset_close_to() != Global.zero_address()
+            ),
+            Reject(),
+        ],
+
+        [Txn.application_id() == Int(0), Approve()],
+        [Txn.application_args.length() == Int(2), opt_in_assets],
+        [Txn.on_completion() == OnComplete.NoOp, swap],
+    )
+
+    return compileTeal(program, Mode.Application, version=5)
+
+def clear_program():
+    return compileTeal(Reject(), Mode.Application, version=5)
+
+def compile_app(algod_client):
+    dump_teal_assembly('akita_token_swapper_approval.teal', approval_program)
+    dump_teal_assembly('akita_token_swapper_clear.teal', clear_program)
+
+    compile_program(algod_client, approval_program(), 'akita_token_swapper_approval.compiled')
+    compile_program(algod_client, clear_program(), 'akita_token_swapper_clear.compiled')
+
+    write_schema(file_path='localSchema',
+                 num_ints=0,
+                 num_bytes=0)
+    write_schema(file_path='globalSchema',
+                 num_ints=3,
+                 num_bytes=0)
